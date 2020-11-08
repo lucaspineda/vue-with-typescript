0b301d51d8dc17bfa878fff4280bdb6a81e3e500
lucaspineda <lucas.pineda@hotmail.com>
2020-11-07 23:53:21 -0300

    firts commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
@@ -10,9 +8,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+  components: {},
 })
 export default class App extends Vue {}
 </script>
